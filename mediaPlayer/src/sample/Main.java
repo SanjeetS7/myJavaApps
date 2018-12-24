f56fd53bcd167a33018368271679ed7565c71b8c
Sanjeet Shukla <45354582+SanjeetS7@users.noreply.github.com>
2018-12-24 19:51:57 +0000

    Add files via upload

---
+++ mediaPlayer/src/sample/Main.java
@@ -0,0 +1,64 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.net.MalformedURLException;
+
+public class Main extends Application {
+
+    Player player;
+    FileChooser fileChooser;
+    @Override
+    public void start(final Stage primaryStage) throws Exception{
+
+        MenuItem open = new MenuItem("Open");
+        Menu file = new Menu("File");
+        javafx.scene.control.MenuBar menu = new javafx.scene.control.MenuBar();
+
+        file.getItems().add(open);
+        menu.getMenus().add(file);
+        fileChooser = new FileChooser();
+
+        open.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                player.player.pause();
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if(file != null) {
+                    try {
+                        player = new Player(file.toURI().toURL().toExternalForm());
+                        player.setTop(menu);
+                        Scene scene = new Scene(player, 720, 535, Color.BLACK);
+                        primaryStage.setScene(scene);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+
+        player = new Player("file:///home/esposito/Videos/izombie.102.hdtv-lol.mp4");
+        player.setTop(menu);
+        Scene scene = new Scene(player, 720, 535, Color.BLACK);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
